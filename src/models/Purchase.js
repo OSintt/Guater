2797094a7da693ac067c0806adc4fb12b35ff2fe
OSintt <osintaxis@gmail.com>
2023-01-13 13:52:40 +0000

    fix

--- src/models/Purchase.js
+++
@@ -1,8 +0,0 @@
-import { Schema, Types, model } from 'mongoose';
-
-const PurchaseSchema = new Schema({
-    date: Date,
-    charge: Number,
-});
-
-export default model('Purchase', PurchaseSchema);
