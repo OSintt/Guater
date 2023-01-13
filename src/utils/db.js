49820ad543c2469eaa8e340033ec5b4db8f846f3
Cosmin <75861657+cosmind-rusu@users.noreply.github.com>
2023-01-13 10:17:52 +0100

    REACT INTERFACE

--- src/utils/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-import { config } from 'dotenv';
-
-config();
-
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('Servicio de MongoDB corriendo!'))
-    .catch(e => console.log(e));
